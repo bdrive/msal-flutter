3b7fc8a90fda1315fa504696a706da46e8b36007
fabio.conceicao <fsconceicao@hotmail.com>
2021-06-02 13:35:39 -0300

    update  version msal 1.1.18 fix alerts version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   
   static const String _authority = "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize";
-  static const String _clientId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+  static const String _clientId = "xxxxxxxxxxxxxxxxxxxxx";
   
   String _output = 'NONE';
   static const List<String> kScopes = [
