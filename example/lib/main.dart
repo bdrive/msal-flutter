242eb855cb469488072fb951dfdf0b858683695c
ysh <ysh@bdrive.com>
2021-08-19 12:34:49 +0900

    Updated main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'dart:async';
 import 'package:msal_flutter/msal_flutter.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -12,88 +11,108 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
   static const String _authority = "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize";
   static const String _clientId = "xxxxxxxxxxxxxxxxxxxxx";
-  
+
   String _output = 'NONE';
+  String _accessToken = 'NONE';
+  String _accountId = 'NONE';
+
   static const List<String> kScopes = [
     "https://graph.microsoft.com/user.read",
     "https://graph.microsoft.com/Calendars.ReadWrite",
   ];
 
-   PublicClientApplication? pca;
+  PublicClientApplication? pca;
 
-  Future<void> _acquireToken() async{
-    if(pca == null){
-      pca = await PublicClientApplication.createPublicClientApplication(clientId: _clientId, authority: _authority);
+  Future<void> _acquireToken() async {
+    if (pca == null) {
+      pca = await PublicClientApplication.createPublicClientApplication(
+          clientId: _clientId, authority: _authority);
     }
 
-    String res;
-    try{
-      res = await pca!.acquireToken(scopes: kScopes);
+    Account res;
+    String error = 'NONE';
+    try {
+      res = (await pca!.acquireToken(scopes: kScopes));
+      setState(() {
+        _output = res.accessToken.substring(0, 40) + "...";
+        _accessToken = res.accessToken;
+        _accountId = res.accountId;
+      });
     } on MsalUserCancelledException {
-      res = "User cancelled";
+      error = "User cancelled";
     } on MsalNoAccountException {
-      res = "no account";
+      error = "no account";
     } on MsalInvalidConfigurationException {
-      res = "invalid config";
+      error = "invalid config";
     } on MsalInvalidScopeException {
-      res = "Invalid scope";
-    }on MsalException {
-      res = "Error getting token. Unspecified reason";
+      error = "Invalid scope";
+    } on MsalException {
+      error = "Error getting token. Unspecified reason.";
     }
 
     setState(() {
-      _output = res;
+      if (error != 'NONE') {
+        _output = error;
+      }
     });
   }
 
   Future<void> _acquireTokenSilently() async {
-    if(pca == null){
-      pca = await PublicClientApplication.createPublicClientApplication(clientId: _clientId, authority: _authority);
+    if (pca == null) {
+      pca = await PublicClientApplication.createPublicClientApplication(
+          clientId: _clientId, authority: _authority);
     }
-    
-    String res;
-    try
-    {
-      res = await pca!.acquireTokenSilent(scopes: kScopes);
+
+    Account res;
+    String error = 'NONE';
+    try {
+      res =
+          await pca!.acquireTokenSilent(scopes: kScopes, accountId: _accountId);
+      setState(() {
+        _output = "silent - " + res.accessToken.substring(0, 40) + "...";
+        _accessToken = res.accessToken;
+        _accountId = res.accountId;
+      });
     } on MsalUserCancelledException {
-      res = "User cancelled";
+      error = "User cancelled";
     } on MsalNoAccountException {
-      res = "no account";
+      error = "no account";
     } on MsalInvalidConfigurationException {
-      res = "invalid config";
+      error = "invalid config";
     } on MsalInvalidScopeException catch (e) {
-      res = "Invalid scope: ${e.errorMessage}";
-    }on MsalException {
-      res = "Error getting token silently!";
+      error = "Invalid scope: ${e.errorMessage}";
+    } on MsalException {
+      error = "Error getting token silently!";
     }
 
     setState(() {
-      _output = res;
+      if (error != 'NONE') {
+        _output = error;
+      }
     });
   }
 
   Future _logout() async {
     print("called logout");
-    if(pca == null){
-      pca = await PublicClientApplication.createPublicClientApplication(clientId: _clientId, authority: _authority);
+    if (pca == null) {
+      pca = await PublicClientApplication.createPublicClientApplication(
+          clientId: _clientId, authority: _authority);
     }
 
-    print("pca is not null");
     String res;
-    try{
-      await pca!.logout();
+    try {
+      await pca!.logout(accountId: _accountId);
       res = "Account removed";
     } on MsalException {
-      res = "Error signing out";
-    } on PlatformException catch (e){
+      res = "MsalException - Error signing out";
+    } on PlatformException catch (e) {
       res = "some other exception ${e.toString()}";
     }
 
     print("setting state");
-    setState((){
+    setState(() {
       _output = res;
     });
   }
@@ -103,22 +122,25 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Microsoft Azure'),
         ),
         body: Center(
           child: Column(
             children: <Widget>[
-              ElevatedButton(onPressed: _acquireToken,
-                child: Text('AcquireToken()'),),
-              ElevatedButton(onPressed: _acquireTokenSilently,
-                child: Text('AcquireTokenSilently()')),
-              ElevatedButton(onPressed: _logout,
-                child: Text('Logout')),
-              Text( _output),
+              ElevatedButton(
+                onPressed: _acquireToken,
+                child: Text('AcquireToken()'),
+              ),
+              ElevatedButton(
+                  onPressed: _acquireTokenSilently,
+                  child: Text('AcquireTokenSilently()')),
+              ElevatedButton(onPressed: _logout, child: Text('Logout')),
+              Text("output: " + _output),
+              Text("accountId: " + _accountId),
             ],
           ),
         ),
       ),
     );
-  }
+  
 }
